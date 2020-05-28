3983937cc73979ca966a40f4b390714acacb6c69
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-05-29 00:18:08 +0530

    Added POST and GET Request from FrontEnd

--- frontend/src/shared/components/Forms/CreateContact.js
+++ frontend/src/shared/components/Forms/CreateContact.js
@@ -4,6 +4,53 @@ import "./CreateContact.css";
 const CreateContact = () => {
   const [dataRecieved, setDataRecieved] = useState(false);
   const [data, setData] = useState([]);
+  const [formDataName, setFormDataName] = useState("");
+  const [formDataDate, setFormDataDate] = useState("");
+  const [formDataPhone, setFormDataPhone] = useState("");
+  const [formDataEmail, setFormDataEmail] = useState("");
+  const [submitClicked, setSubmitClicked] = useState(false);
+  //Handling the input form data
+  const HandleDataName = (data) => {
+    setFormDataName(data);
+    console.log(data);
+  };
+  const HandleDataDate = (data) => {
+    setFormDataDate(data);
+    console.log(data);
+  };
+  const HandleDataPhone = (data) => {
+    setFormDataPhone(data);
+    console.log(data);
+  };
+  const HandleDataEmail = (data) => {
+    setFormDataEmail(data);
+    console.log(data);
+  };
+  //For Sending the data
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const responseData = await fetch("http://localhost:5000/api/contacts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: formDataEmail.toString(),
+          DOB: "02-12-2000",
+          Phone: formDataPhone.toString(),
+          name: formDataName.toString(),
+          creator: "5ecfbe181b011937684d72f3",
+        }),
+      });
+      console.log(responseData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //For getting the data
   useEffect(async () => {
     const data = await fetch(
       "http://localhost:5000/api/contacts/user/5ecfbe181b011937684d72f3",
@@ -45,54 +92,75 @@ const CreateContact = () => {
   const [contact, newContact] = useState({});
 
   return (
-    <div className="main-content">
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" />
-          <Form.Text className="text-muted">This Field is Important.</Form.Text>
-        </Form.Group>
+    <>
+      <div className="main-content">
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              onChange={(event) => {
+                HandleDataName(event.target.value);
+              }}
+              type="text"
+              placeholder="Enter Name"
+            />
+            <Form.Text className="text-muted">
+              This Field is Important.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>DOB</Form.Label>
-          <Form.Control type="date" placeholder="dd/mm/yyyy" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Row>
-            <Col>
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control placeholder="Enter Phone Number" />
-            </Col>
-            <Col>
-              <Form.Label>Add Number</Form.Label>
-              <br />
-              <Button variant="outline-success">Add</Button>
-            </Col>
-          </Form.Row>
-        </Form.Group>
-        <Form.Group>
-          <Form.Row>
-            <Col>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter Email" />
-            </Col>
-            <Col>
-              <Form.Label>Add Email</Form.Label>
-              <br />
-              <Button variant="outline-success">Add</Button>
-            </Col>
-          </Form.Row>
-        </Form.Group>
-        <Button
-          variant="primary"
-          type="submit"
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
-          Submit
-        </Button>
-      </Form>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>DOB</Form.Label>
+            <Form.Control
+              type="date"
+              onChange={(event) => {
+                HandleDataDate(event.target.value);
+              }}
+              placeholder="dd/mm/yyyy"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Row>
+              <Col>
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  onChange={(event) => {
+                    HandleDataPhone(event.target.value);
+                  }}
+                  placeholder="Enter Phone Number"
+                />
+              </Col>
+              <Col>
+                <Form.Label>Add Number</Form.Label>
+                <br />
+                <Button variant="outline-success">Add</Button>
+              </Col>
+            </Form.Row>
+          </Form.Group>
+          <Form.Group>
+            <Form.Row>
+              <Col>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  onChange={(event) => {
+                    HandleDataEmail(event.target.value);
+                  }}
+                  type="email"
+                  placeholder="Enter Email"
+                />
+              </Col>
+              <Col>
+                <Form.Label>Add Email</Form.Label>
+                <br />
+                <Button variant="outline-success">Add</Button>
+              </Col>
+            </Form.Row>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
       <div className="display-content">
         <Accordion>
           {!dataRecieved &&
@@ -173,7 +241,7 @@ const CreateContact = () => {
             })}
         </Accordion>
       </div>
-    </div>
+    </>
   );
 };
 
