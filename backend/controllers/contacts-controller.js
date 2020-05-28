05336d9a13c739ab86e2a55fc3c84cc491b8b6e6
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-05-28 13:52:46 +0530

    Added some more functionality to contact controller

--- backend/controllers/contacts-controller.js
+++ backend/controllers/contacts-controller.js
@@ -1,11 +1,14 @@
-//Added Dummy contact list for testing purposes
 const HttpError = require("../models/http-error");
+
+const { validationResult } = require("express-validator");
+
+//Added Dummy contact list for testing purposes
 
 let DUMMY_CONTACTS = [
   {
     name: "tester",
     DOB: "28/05/2020",
-    Phone: ["9999999999"],
+    Phone: "9999999999",
     email: "testing@test.com",
     id: "c1",
   },
@@ -18,6 +21,10 @@ const viewContacts = (req, res, next) => {
 
 //post
 const addContact = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    next(new HttpError("Invalid inputs passed, please check your data.", 422));
+  }
   const { name, DOB, Phone, email } = req.body;
 
   const createdContact = {
@@ -35,23 +42,29 @@ const addContact = (req, res, next) => {
 //post/:cid
 const updateContact = (req, res, next) => {
   const contactId = req.params.cid;
-  console.log(contactId);
-  const { name, DOB, Phone, email } = req.body;
-
+  const { Phone, email } = req.body;
+  if (!DUMMY_CONTACTS.find((c) => c.id === contactId)) {
+    throw new HttpError("Could not find the Specific Contact");
+  }
   const updatedContact = {
     ...DUMMY_CONTACTS.find((c) => c.id === contactId),
   };
-  console.log("Dummy", updatedContact);
+
   const contactIndex = DUMMY_CONTACTS.findIndex((c) => c.id === contactId);
-  console.log(contactIndex);
-  updatedContact.name = name;
+
+  console.log("first", updatedContact);
   updatedContact.email = email;
-  updatedContact.DOB = DOB;
   updatedContact.Phone = Phone;
+  console.log("second", updatedContact);
 
-  DUMMY_CONTACTS[contactIndex] = updatedContact;
-  console.log(updatedContact);
-
+  let newEmailArray = [];
+  let newPhoneArray = [];
+  newEmailArray.push(updatedContact.email);
+  newEmailArray.push(DUMMY_CONTACTS[contactIndex].email);
+  DUMMY_CONTACTS[contactIndex].email = newEmailArray;
+  newPhoneArray.push(updatedContact.Phone);
+  newPhoneArray.push(DUMMY_CONTACTS[contactIndex].Phone);
+  DUMMY_CONTACTS[contactIndex].Phone = newPhoneArray;
   res.status(201).json({ message: "Successfully Updated" });
 };
 
--- backend/routes/contacts-routes.js
+++ backend/routes/contacts-routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { check } = require("express-validator");
 const router = express.Router();
 
 //Contacts Controller section
@@ -7,7 +7,11 @@ const contactsController = require("../controllers/contacts-controller");
 
 //Performing get,post,patch,delete methods
 router.get("/", contactsController.viewContacts);
-router.post("/", contactsController.addContact);
+router.post(
+  "/",
+  [check("name").not().isEmpty(), check("Phone").not().isEmpty()],
+  contactsController.addContact
+);
 router.patch("/:cid", contactsController.updateContact);
 router.delete("/:cid", contactsController.deleteContact);
 
